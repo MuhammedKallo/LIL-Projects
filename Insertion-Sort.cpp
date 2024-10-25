e8d0df9c20cdeb5985876fa78071fae340782277
Muhammed Kallo <massagekelo@gmail.com>
2024-10-25 18:56:54 +0300

    Add files via upload

---
+++ Insertion-Sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void printing(int *array, int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << array[i] << " ";
+    cout << endl;
+}
+
+void insertionsort(int *array, int size)
+{
+    int key, j, i;
+    for(i = 1; i < size; i++)
+    {
+        key = array[i];
+        j = i;
+    
+        while(j > 0 && array[j -1] > key)
+        {
+            array[j] = array[j - 1];
+            j--;
+        }
+        array[j] = key;
+    }   
+}
+    
+int main()
+{
+    int size;
+    cout << "Enter the size of the array : ";
+    cin >> size;
+
+    int array[size];
+    for(int i = 0; i < size; i++)
+        cin >> array[i];
+
+    insertionsort(array, size);
+    printing(array, size);
+
+    return (0);
+}
